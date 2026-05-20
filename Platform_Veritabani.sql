e686e9cca41735fe901264208eaa6ac4950978c0
miraysanver <miraysanver04@gmail.com>
2026-05-20 19:08:58 +0300

    Initial commit: Temel tablolar eklendi

---
+++ Platform_Veritabani.sql
@@ -0,0 +1,28 @@
+
+CREATE TABLE Kullanicilar (
+    KullaniciID INT IDENTITY(1,1), 
+    Ad VARCHAR(50) NOT NULL,
+    Soyad VARCHAR(50) NOT NULL,
+    Eposta VARCHAR(100) NOT NULL,
+    Telefon VARCHAR(15) NOT NULL,
+    Rol VARCHAR(20) NOT NULL, 
+    Bakiye DECIMAL(10, 2) DEFAULT 0.00,
+    IsVerified BIT DEFAULT 0, 
+    IsActive INT DEFAULT 1, 
+    
+    CONSTRAINT PK_Kullanicilar PRIMARY KEY (KullaniciID), 
+    CONSTRAINT UQ_KullaniciEposta UNIQUE (Eposta), 
+    CONSTRAINT UQ_KullaniciTelefon UNIQUE (Telefon), 
+    CONSTRAINT CHK_KullaniciBakiye CHECK (Bakiye >= 0)
+);
+
+CREATE TABLE Restoranlar (
+    RestoranID INT IDENTITY(1,1), 
+    RestoranAdi VARCHAR(100) NOT NULL,
+    Adres TEXT NOT NULL,
+    RestoranPuani DECIMAL(2, 1) DEFAULT 1.0,
+    IsActive INT DEFAULT 1,
+    
+    CONSTRAINT PK_Restoranlar PRIMARY KEY (RestoranID), 
+    CONSTRAINT CHK_RestoranPuani CHECK (RestoranPuani BETWEEN 1.0 AND 5.0) 
+);
